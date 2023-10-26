ed02e4b1e6acfe7871219b515b89d171959b63f4
Oscar Lin <heartzephyr@gmail.com>
2023-10-26 13:23:13 +0200

    Correction serviceMessagerie

--- Serveur/src/examples/ServiceMessagerie.java
+++ Serveur/src/examples/ServiceMessagerie.java
@@ -5,11 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import serveur.Service;
 
 public class ServiceMessagerie implements Service, AutoCloseable{
-private final Socket client;
+	private final Socket client;
+	private static Map<String, ArrayList<String>> messageMap;
+
+	static {
+		messageMap = new HashMap<>();
+	}
 	
 	public ServiceMessagerie(Socket socket) {
 		client = socket;
@@ -20,24 +28,67 @@ private final Socket client;
 		try {
 			BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
 			PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
-			out.println("Entrez le pseudo du destinataire :");
-			String nomDestinataire = in.readLine();
-			out.println("Entrez votre message : ");
-			String message = in.readLine();
-			
-			client.close();
+
+			while(true) {
+				out.println("1: Consultation des messages ## 2: Envoie d'un message");
+				String res = in.readLine();
+				switch (res) {
+					case "1":
+						consultationMessage(in,out);
+						break;
+					case "2":
+						envoieMessage(in,out);
+						break;
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 	}
 
+	private void envoieMessage(BufferedReader in, PrintWriter out) throws IOException {
+		out.println("Entrez le pseudo du destinataire :");
+		String nomDestinataire = in.readLine();
+		out.println("Entrez votre message : ");
+		String message = in.readLine();
+		addMessage(nomDestinataire,message);
+	}
+
+	private void consultationMessage(BufferedReader in, PrintWriter out) throws IOException {
+		out.println("Entrez votre pseudo :");
+		String pseudo = in.readLine();
+		out.println(getMessages(pseudo) + "Faites une entrée clavier pour revenir à l'accueil");
+		in.readLine();
+	}
+
 	@Override
 	public void close() throws Exception {
-		// TODO Auto-generated method stub
 		client.close();
 	}
-	
+
+	public void addMessage(String login, String message) {
+		if(this.messageMap.get(login) != null) {
+			this.messageMap.get(login).add(message);
+		}
+		else {
+			ArrayList<String> mes = new ArrayList<>();
+			mes.add(message);
+			this.messageMap.put(login, mes);
+		}
+	}
+
+	public String getMessages(String login) {
+		StringBuilder str = new StringBuilder("Liste de messages : ##");
+		for(int i = 0; i < this.messageMap.get(login).size(); i++) {
+			str.append(this.messageMap.get(login).get(i));
+			str.append("##");
+		}
+
+
+		return str.toString();
+	}
+
 	public static String toStringue() {
 		return "Messagerie";
 	}
--- Serveur/src/serveur/ServiceMessage.java
+++ Serveur/src/serveur/ServiceMessage.java
@@ -7,41 +7,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class ServiceMessage {
-	private static Map<String, ArrayList<String>> messageMap;
+
 	
-	static {
-		messageMap = new HashMap<>();
-	}
-	
-	public void addMessage(String login, String message) {
-		if(this.messageMap.get(login) != null) {
-			this.messageMap.get(login).add(message);
-		}
-		else {
-			ArrayList<String> mes = new ArrayList<>();
-			mes.add(message);
-			this.messageMap.put(login, mes);
-		}
-	}
-	
-	public String getMessages(String login) {
-		StringBuilder str = new StringBuilder();
-		for(int i = 0; i < this.messageMap.get(login).size(); i++) {
-			str.append(this.messageMap.get(login).get(i));
-		}
-		return str.toString();
-	}
-	
-	public String toStringue() {
-		StringBuilder str = new StringBuilder();
-		for(Entry<String, ArrayList<String>> entry : messageMap.entrySet()) {
-		    str.append("Liste de message pour l'utilisateur " + entry.getKey() + " : ");
-		    for(int i = 0; i < messageMap.get(entry.getKey()).size(); i++){
-		    	str.append(messageMap.get(entry.getKey()).get(i));
-		    	str.append(" ");
-		    }
-		    str.append("##");
-		}		
-		return str.toString();
-	}
+
 }
